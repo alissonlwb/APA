0091f6f34e79364c847d372c716016c82e238800
Alisson Lacerda <alissonlacerda@Alissons-MBP.home>
2019-02-13 12:07:12 -0300

    teste na main

--- T3/sorting_t3.cpp
+++ T3/sorting_t3.cpp
@@ -10,25 +10,28 @@ using namespace std;
 
 // funcoes pro counting sort
 
-void Counting_Sort(int number[], int output[], int size, int k){
+void counting_sort(int number[], int output[], int size, int k){
 
 	int count[size];
 
-
+	// zera o vetor auxiliar
 	for (int i = 0; i < k; i++){
 		count[i] = 0;
 	}
 
-	for (int i=0; i < k; i++){
+	// contagem dos numeros
+	for (int i = 0; i < k; i++){
 		count[number[i]] = count[number[i]]+1;
 
 	}
 
-	for (int i=1; i < size; i++){
+	// contagem dos numeros menores ou iguais
+	for (int i = 1; i < size; i++){
 		count[i] = count[i] + count[i-1];
 	}
 
-	for (int i=k-1; i >= 0; i--){
+	// percorre de tras pra frente pra preencher vetor auxiliar e de saida
+	for (int i = k-1; i >= 0; i--){
 		output[count[number[i]]-1] = number[i];
 		count[number[i]] = count[number[i]]-1;
 
@@ -41,5 +44,58 @@ void print_values(int number[], int size){
 	for (int i = 0; i < size; ++i){
         cout << "Pos(" << i+1 << ") ; Num(" << number[i] << ")" << endl;
     }
+
+}
+
+int main(){
+
+	string input;
+	int size = 100000;
+	int array[100000];// caso maximo 100000
+	int linhas = 0;// indice das linhas do array
+	int option;// opcao pra selecionar insertion ou selection sort
+
+	cout << "Digite 1 para COUNTING SORT ou 2 pra RADIX SORT: " << endl;
+	cin >> option;
+
+	cout << "Arquivo com os casos de teste: " << endl;
+	cin >> input;
+
+	// leitura do arquivo
+	ifstream file;
+	file.open(input);
+	char output[1000];
+
+	if (file.is_open()){
+		while (!file.eof()){
+			file >> output;
+			array[linhas] = atoi(output);// cada numero vai pra uma linha do array
+			linhas++;// incrementa linha pra pegar o proximo numero e por na proxuma linha do array
+		}
+	}
+
+	// fecha arquivo
+	file.close();
+
+	// vetor de saida com numero de linhas do arquivo
+	int output[linhas];
+
+	// menu switch case
+	switch (option){
+		case 1:
+			counting_sort(array, output, size, linhas)
+			print_values(output, linhas);
+			break;
+		case 2:
+			//merge_sort(array, linhas);
+			//print_values(array, linhas);
+			break;
+		default:
+			cout << "Opcao invalida, digite 1 ou 2";
+			break;
+
+	}
+	
+	
 
 }
