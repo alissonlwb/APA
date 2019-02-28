a59f5f645bdcbf94d13dd01563c5cfa8bb32ab6b
Alisson Lacerda <alissonlacerda@Alissons-MBP.home>
2019-02-28 09:23:26 -0300

    comentarios e funcao heapsort

--- T4/sorting_t4.cpp
+++ T4/sorting_t4.cpp
@@ -8,24 +8,55 @@
 
 using namespace std;
 
+// cria subarvore com tamanho "n" e raíz em "i" 
 void heap(int input[], int n, int i)
 {
-   int maior = i;
-   int esquerda = 2*i + 1;
-   int direita = 2*i + 2;
+	int maior = i;// maior vai ser a raíz
+	int esquerda = 2*i + 1;
+	int direita = 2*i + 2;
+	int aux = 0;// utilizado pra troca
 
-   if (esquerda < n && input[esquerda] > input[maior])
-     maior = esquerda;
+	// se folha da esquerda for maior que a raiz, maior = esquerda 
+	if (esquerda < n && input[esquerda] > input[maior])
+		maior = esquerda;
 
-   if (direita < n && input[direita] > input[maior])
-     maior = direita;
+	// se folha da direita for maior que a raiz, maior = direita 
+	if (direita < n && input[direita] > input[maior])
+		maior = direita;
 
-   if (maior != i)
-   {
-     swap(input[i], input[maior]);
-     heap(input, n, maior);
-   }
+	// por fim, testa se o maior nao é raiz
+	if (maior != i)
+	{
+		// troca os valores de input[i] e input[maior]		
+		aux = input[i];
+		input[i] = input[maior];
+		input[maior] = aux;
+		// faz recursao do heap pra subarvore tamanho "n" e raiz "maior"
+		heap(input, n, maior);
+	}
 }
+
+// funcao pro heapsort
+void heapsort(int input[], int n)
+{
+	int aux = 0;// utilizado pra troca
+
+	// ordena o heap
+	for (int i = n / 2-1; i >= 0; i--)
+		heap(input, n, i);
+
+	// retira os elementos do heap um por vez
+	for (int i=n-1; i>=0; i--)
+	{
+		// troca o nó "i" atual pelo final "0"
+		aux = input[0];
+		input[0] = input[i];
+		input[i] = aux;
+		// faz recursao do heap com tamanho "i" e raíz "0"
+		heap(input, i, 0);
+	}
+}
+
 
 int main(){
 
