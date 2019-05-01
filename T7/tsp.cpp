9a17c1c018f3c8db75f677e8d741880a6426a723
Alisson Lacerda <alissonlacerda@Alissons-MacBook-Pro.local>
2019-05-01 08:24:57 -0300

    2opt e swap feitos

--- T7/tsp.cpp
+++ T7/tsp.cpp
@@ -26,11 +26,60 @@ int euclidian_distance(double xi, double yi, double xj, double yj)
 	round(distance);                  
 
 	return distance;
+}
 
+// calcula custo dado a matriz de adjacencia e um caminho
+int calculate_cost(int **adj_matrix, vector<int> route) 
+{
+    int cost = 0;
+
+    for(int i = 0; i < route.size() - 1; i++) 
+    {
+    	// acumula custo da adj_matrix com os indices da rota [i][i+1]
+        cost += adj_matrix[route[i]][route[i + 1]];
+    }
+
+    // retorna o custo
+    return cost;
+}
+
+// inverte o miolo entre "inicio e final" no 2-opt
+vector<int> invert(vector<int> to_invert, int inicio, int final)
+{
+	// EXEMPLO:
+	//
+	// to_invert = 	|0|3|2|4|1|5|
+	// temp =		|0|3|2|4|1|5|
+	// indices (inicio = 1; final = 4)
+	//
+	// i = 1; j = 4
+	// to_invert = 	|0|1|2|4|1|5|
+	// temp =		|0|3|2|4|1|5|
+
+	// i = 2; j = 3
+	// to_invert = 	|0|1|4|4|1|5|
+	// temp =		|0|3|2|4|1|5|
+	
+	// i = 3; j = 2
+	// to_invert = 	|0|1|4|2|1|5|
+	// temp =		|0|3|2|4|1|5|
+
+	// i = 4; j = 1.   .     .
+	// to_invert = 	|0|1|4|2|3|5| (miolo invertido)
+	// temp =		|0|3|2|4|1|5|
+
+	vector<int> temp;
+	temp = to_invert;
+
+	for(int i = inicio, j = final; i <= final; i++, j--){
+		to_invert[i] = temp[j];
+	}
+
+	return to_invert;
 }
 
 // heuristica de construcao do vizinho mais proximo 
-void nearest_neighbour(int *adj_matrix)
+vector<int> nearest_neighbour(int **adj_matrix)
 {
 	// vetor booleano pra marcar os visitados 
 	vector<bool> visited_node(1500, false);
@@ -48,22 +97,21 @@ void nearest_neighbour(int *adj_matrix)
 	int past_visited = 0;
 	
 	// lista para os nos do caminho
-	vector<int> path;
-	path.push_back(curr_node);
+	vector<int> route;
+	route.push_back(curr_node);
 
-	// enquanto o tamanho do vetor path for menor que 1500
-	while(path.size() < 1500)
+	// enquanto o tamanho do vetor route for menor que 1500
+	while(route.size() < 1500)
 	{
 		// min = maior valor possivel 
 		int min = __INT_MAX__;
 		for(int i = 0; i < 1500; ++i)
 		{
 			// se nó i nao tiver sido visitado, e o nó atual for menor que o minimo
-			// adj_matrix[curr_node][i]
-			if((!visited_node[i]) && (adj_matrix[curr_node*1500 + i] < min))
+			if((!visited_node[i]) && (adj_matrix[curr_node][i] < min))
 			{
 				// minimo recebe o no atual
-				min = adj_matrix[curr_node*1500 + i];
+				min = adj_matrix[curr_node][i];
 				// visitado anterior recebe i
 				past_visited = i;
 			}
@@ -79,137 +127,126 @@ void nearest_neighbour(int *adj_matrix)
 		curr_node = past_visited;
 
 		// coloca o anterior na lista do caminho
-		path.push_back(past_visited);
+		route.push_back(past_visited);
 	}
 
+	cout<<"Heuristica do vizinho mais proximo:\n";
 	// imprime o custo total
 	cout<<"Custo minimio total: "<<cost<<"\n";
 
 	cout<<"Caminho: ";
 	// imprime o caminho
-	for (int i = 0; i < path.size(); ++i)
+	for (int i = 0; i < route.size(); ++i)
 	{
-		cout<<path[i]<<", ";
+		cout<<route[i]<<", ";
 	}
 	// imprime o nó inicial novamente
-	cout<<"0";
+	cout<<"0\n\n";
+
+	return route;
 }
 
-int calcutale_cost()
+// movimento de vizinhanca swap
+vector<int> swap(int **adj_matrix, vector<int> route)
 {
-	int cost = 0;
-
+	// vetor temp faz copia da rota original
+	vector<int> temp;
+	temp = route;
 	
-}
+	// custo inicial é o custo da rota original
+	int init_cost = calculate_cost(adj_matrix, route);
+	// custo temporario
+	int temp_cost;
 
+	for(int i = 1; i < 1500; i++){
 
+		// reseta rota temporaria
+		temp = route;
 
-// 2optSwap(route, i, k) {
-//      1. take route[1] to route[i-1] and add them in order to new_route
-//      2. take route[i] to route[k] and add them in reverse order to new_route
-//      3. take route[k+1] to end and add them in order to new_route
-//      return new_route;
-//  }
+		for(int j = i + 1; j < 1500; j++){
+			int aux = temp[i];  // guarda valor de temp[i] em aux
+			temp[i] = temp[j];  // temp[i] recebe temp[j]
+			temp[j] = aux;		// temp[j] recebe aux que guarda temp[i]
 
-// Here is an example of the above with arbitrary input:
+			// calcula custo pra rota temporaria com swap feito
+			temp_cost = calculate_cost(adj_matrix, temp);
 
-// example route: A ==> B ==> C ==> D ==> E ==> F ==> G ==> H ==> A  
-//    example i = 4, example k = 7  
-//    new_route:  
-//        1. (A ==> B ==> C)  
-//        2. A ==> B ==> C ==> (G ==> F ==> E ==> D)  
-//        3. A ==> B ==> C ==> G ==> F ==> E ==> D (==> H ==> A)
-
-// recebe o vetor com o caminho antigo e dois indices pra delimitar o "miolo" a ser trocado
-vector<int> two_opt_swap(vector<int> old_path, int i, int k)
-{
-	// inicializa vetor new_path tamanho 1500 e zera tudo 
-	vector<int> new_path(1500, 0);
-
-	// pega old_path[1] até old_path[i-1] e os adicione em ordem ao vetor new_path
-	for (int j = 1; j <= j-1; ++i)
-	{
-		new_path.push_back(old_path[j]);
-	}
-
-	// pega old_path[i] até old_path[k] e os adicione em na ordem reversa ao vetor new_path
-	for (int x = i; x < k; --x)
-	{
-		new_path.push_back(old_path[x]);
-	}
-
-	// pega old_path[k+1] até old_path[1500] e os adicione em ordem no vetor new_path
-	for (int y = k+1; y < 1500; ++y)
-	{
-		new_path.push_back(old_path[x]);
-	}
-
-	// retorna vetor com caminho novo 
-	return new_path;
-	
-}
-
-// This is the complete 2-opt swap making use of the above mechanism:
-
-//    repeat until no improvement is made {
-//        start_again:
-//        best_distance = calculateTotalDistance(existing_route)
-//        for (i = 0; i < number of nodes eligible to be swapped - 1; i++) {
-//            for (k = i + 1; k < number of nodes eligible to be swapped; k++) {
-//                new_route = 2optSwap(existing_route, i, k)
-//                new_distance = calculateTotalDistance(new_route)
-//                if (new_distance < best_distance) {
-//                    existing_route = new_route
-//                    goto start_again
-//                }
-//            }
-//        }
-//    }
-
-void two_opt(vector<int> old_path, int curr_cost)
-{
-	int best_cost = curr_cost;
-	int new_cost = 0;
-	vector<int> new_path(1500, 0);
-	bool improved = true;
-
-	while(improved)
-	{
-		calcutale_cost(old_path);
-		improved = false;
-		for (int i = 0; i < 1500; ++i)
-		{
-			for (int k = i + 1; k < 1500; ++k)
-			{
-				new_path = two_opt_swap(old_path, i, k);
-				new_cost = calcutale_cost(new_path);
-
-				if(new_cost < best_cost)
-				{
-					old_path = new_path;
-					improved = true;
-				}
+			// se custo da rota temporaria for menor que da original
+			if(temp_cost < init_cost){
+				// custo da rota original recebe custo temporario
+				init_cost = temp_cost;
+				// rota original recebe rota temporaria
+				route = temp;
 			}
 		}
 	}
 
+	cout<<"Movimento de vizinhanca swap:\n";
 	// imprime o custo total
-	cout<<"Custo minimio total: "<<best_cost<<"\n";
+	cout<<"Custo minimio total: "<<init_cost<<"\n";
 
 	cout<<"Caminho: ";
 	// imprime o caminho
-	for (int i = 0; i < path.size(); ++i)
+	for (int i = 0; i < 1500; ++i)
 	{
-		cout<<new_path[i]<<", ";
+		cout<<route[i]<<", ";
 	}
 	// imprime o nó inicial novamente
-	cout<<"0";
+	cout<<"0\n\n";
+
+	return route;	
 }
+
+// movimento de vizinhanca 2-opt
+vector <int> two_opt(int **adj_matrix, vector <int> route)
+{
+	vector <int> temp = route;
+	vector <int> init = route;
+	int temp_cost;
+	int init_cost = calculate_cost(adj_matrix, route);
+
+	for(int i = 1; i < 1500; i++)
+	{
+		for(int j = i + 1; j < 1500; j++)
+		{
+			// inverte o miolo delimitado por i = 1 ; j = 2,3,4,...,1499
+			// inverte o miolo delimitado por i = 2 ; j = 3,2,5,...,1499
+			// inverte o miolo delimitado por i = 3 ; j = 4,5,6,...,1499
+			// ...
+			temp = invert(temp, i, j);
+
+			// calcula custo da rota temporaria 
+			temp_cost = calculate_cost(adj_matrix, temp);
+			// se custo da rota temporaria for menor que custo da rota inicial
+			// custo inicial recebe custo temporario e rota inicial recebe rota temporaria
+			if(temp_cost < init_cost){
+				init_cost = temp_cost;
+				route = temp;
+			}
+			// reseta rota temporaria
+			temp = init;
+		}
+	}
+	cout<<"Movimento de vizinhanca 2-opt:\n";
+	// imprime o custo total
+	cout<<"Custo minimio total: "<<init_cost<<"\n";
+
+	cout<<"Caminho: ";
+	// imprime o caminho
+	for (int i = 0; i < route.size(); ++i)
+	{
+		cout<<route[i]<<", ";
+	}
+	// imprime o nó inicial novamente
+	cout<<"0\n\n";
+
+	return route;
+}
+
 
 
 int main(int argc, char *argv[])
 {
-
 	// array pra posicoes x e y
 	double pos_x[1500];
 	double pos_y[1500];
@@ -217,11 +254,17 @@ int main(int argc, char *argv[])
 	int count = 0;
 	
 	// alocacao dinamica matriz[1500][1500]
-	int *adj_matrix = new int[1500*1500];
+	int **adj_matrix = new int*[1500];
+	for(int i = 0; i < 1500; i++)
+		adj_matrix[i] = new int[1500];
+	for(int i = 0; i < 1500; i++)
+		for(int j = 0; j < 1500; j++)
+			adj_matrix[i][j] = 0;
 
 	// abre o arquivo com os pontos x e y
-	ifstream in("tsp1.txt");
-	// pega 1500 valores
+	ifstream in("tsp3.txt");
+	
+	// pega os 1500 valores
 	while(count!=1500)
 	{
 		// pega os valores de x e y
@@ -234,13 +277,19 @@ int main(int argc, char *argv[])
 	{
 		for (int j = 0; j < 1500; j++)
 		{
-			//adj_matrix[i][j]
-			adj_matrix[i*1500 + j] = euclidian_distance(pos_x[i], pos_y[i], pos_x[j], pos_y[j]);
+			// adj_matrix[i][j] é populada com a distancia euclidiana
+			adj_matrix[i][j] = euclidian_distance(pos_x[i], pos_y[i], pos_x[j], pos_y[j]);
 		}
 	}
 
 	// chama heuristica de construcao do vizinho mais proximo passando a matriz de adjacencia
-	nearest_neighbour(adj_matrix);
+	vector<int> route = nearest_neighbour(adj_matrix);
+
+	// movimento de vizinhanca swap
+	swap(adj_matrix, route);
+
+	// movimento de vizinhanca 2-opt
+	two_opt(adj_matrix, route);
 
 	// deleta a matriz alocada
 	delete[] adj_matrix;
